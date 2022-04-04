b0a5b7061b47f6c67cc5de5c518df4754cf07414
Ilya Yarosh <Ilya.Yarosh@itechart-group.com>
2022-04-04 21:44:35 +0300

    added form and model

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.set('view engine', 'hbs')
 app.set('views', 'views') // second 'views' - folder name with pages in project!
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
