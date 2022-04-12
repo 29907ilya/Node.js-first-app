1c8a969a1bcab797c606e607bb0ebe31d8a66d15
Ilya Yarosh <Ilya.Yarosh@itechart-group.com>
2022-04-12 16:27:03 +0300

    added dynamic cart editing

--- public/app.js
+++ public/app.js
@@ -1,14 +1,49 @@
-document.querySelectorAll('.price').forEach( node => {
-  node.textContent = new Intl.NumberFormat('en-us', {
-    currency: 'usd',
-    style: 'currency'
-  }).format(node.textContent)
-})
+const toCurrency = price => {
+  return new Intl.NumberFormat("en-us", {
+  currency: "usd",
+  style: "currency",
+}).format(price);
 
-const $cart = document.querySelector('#cart')
+}
+
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = toCurrency(node.textContent)
+});
+
+const $cart = document.querySelector("#cart");
 
 if ($cart) {
-  $cart.addEventListener('click', event => {
-    
-  })
+  $cart.addEventListener("click", (event) => {
+    if (event.target.classList.contains("js-remove")) {
+      const id = event.target.dataset.id;
+
+      fetch("/cart/remove/" + id, {
+        method: "delete",
+      })
+        .then((res) => res.json())
+        .then((cart) => {
+          if (cart.courses.length) {
+            const html = cart.courses.map((c) => {
+              return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td>
+                  <button
+                    class="btn btn-small js-remove"
+                    data-id="${c.id}"
+                  >del</button>
+                </td>
+              </tr>
+              `;
+            }).join('')
+
+            $cart.querySelector('tbody').innerHTML = html
+            $cart.querySelector('.price').textContent = toCurrency(cart.price)
+          } else {
+            $cart.innerHTML = "<p>Shopping cart is empty</p>";
+          }
+        });
+    }
+  });
 }
