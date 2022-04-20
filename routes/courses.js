f0a60538515f0477ad6de70891b5e93a2710f23d
Ilya Yarosh <Ilya.Yarosh@itechart-group.com>
2022-04-20 10:57:44 +0300

    compleated model refactoring

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const courses = await Course.getAll()
+  const courses = await Course.find().lean()
   res.render('courses', {
     title: 'Courses',
     isCourses: true,
@@ -15,22 +15,26 @@ router.get('/:id/edit', async (req, res) => {
   if (!req.query.allow) {
     return res.redirect('/')
   }
-
-  const course = await Course.getById(req.params.id)
+ 
+  const course = await Course.findById(req.params.id).lean()
 
   res.render('course-edit', {
     title: `Edit ${course.title} course`,
     course
   })
-})
+}) 
 
 router.post('/edit', async (req, res) => {
-  await Course.update(req.body)
+  const {id} = req.body
+  delete req.body.id
+  await Course.findByIdAndUpdate(id, req.body).lean()
   res.redirect('/courses')
 })
 
 router.get('/:id', async (req, res) => {
-  const course = await Course.getById(req.params.id)
+  console.log(req.params.id);
+  const course = await Course.findById(req.params.id).lean()
+
   res.render('course', {
     layout: 'empty',
     title: `Course ${course.title}`,
@@ -38,13 +42,13 @@ router.get('/:id', async (req, res) => {
   })
 })
 
-router.post('/', async (req, res) => {
-  const courses = await Course.getAll()
-  res.render('courses', {
-    title: 'Courses',
-    isCourses: true,
-    courses
-  })
-})
+// router.post('/', async (req, res) => {
+//   const courses = await Course.getAll()
+//   res.render('courses', {
+//     title: 'Courses',
+//     isCourses: true,
+//     courses
+//   })
+// })
 
 module.exports = router
